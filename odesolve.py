637578b4f8b9c563fa764bb9b46fc8d353bdf380
zuglyz <102162074+zuglyz@users.noreply.github.com>
2022-08-03 08:13:40 +0100

    Add files via upload

---
+++ odesolve.py
@@ -0,0 +1,27 @@
+# odesolve.py
+#
+# Author: <insert name>
+# Date:   <insert date>
+# Description: <insert description>
+#
+# You should fill out the code for the functions below so that they pass the
+# tests in test_odesolve.py
+
+def euler(f, x, t, h):
+    """Perform one step of the Euler method"""
+    pass
+
+
+def rk4(f, x, t, h):
+    """Perform one step of the RK$ method"""
+    pass
+
+
+def solveto(f, x1, t1, t2, hmax, method=euler):
+    """Use many steps of method to get from x1,t1 to x2,t2"""
+    pass
+
+
+def odesolve(f, X0, t, hmax, method=euler):
+    """Compute the solution at different values of t"""
+    pass
