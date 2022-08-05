271c053b422ccfd99a2eec7ae58ba68b3c0427b8
zuglyz <102162074+zuglyz@users.noreply.github.com>
2022-08-05 04:32:28 +0100

    Update odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -149,16 +149,3 @@ def rk4 (a0, b0, bc, c):
 rk4(a0, b0, bc, c)
 
 
-
-# solveto function
-
-def solveto(
-
-def solveto(f, x1, t1, t2, hmax, method=euler):
-    """Use many steps of method to get from x1,t1 to x2,t2"""
-    pass
-
-
-def odesolve(f, X0, t, hmax, method=euler):
-    """Compute the solution at different values of t"""
-    pass
