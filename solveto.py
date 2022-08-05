7824b9d75041b3421720772fab396d8135cf9f7e
zuglyz <102162074+zuglyz@users.noreply.github.com>
2022-08-05 04:32:52 +0100

    Create solveto.py

---
+++ solveto.py
@@ -0,0 +1,12 @@
+# solveto function
+
+def solveto(
+
+def solveto(f, x1, t1, t2, hmax, method=euler):
+    """Use many steps of method to get from x1,t1 to x2,t2"""
+    pass
+
+
+def odesolve(f, X0, t, hmax, method=euler):
+    """Compute the solution at different values of t"""
+    pass
